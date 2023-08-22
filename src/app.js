9304af3c3daae227986e42f6aa28b8b2f6685202
Huseyin Ozdemir <huseyinozdemir@huseyin-air.mynet>
2023-08-22 20:52:57 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+
+const app = express();
+
+
+//start with async
+async function startApp() {
+
+
+
+    //body parser
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    const cookieParser = require('cookie-parser');
+
+    //cookie parser
+    app.use(cookieParser());
+
+
+    //wiew engine
+
+
+    //app routes
+
+
+    //404 not found
+    app.use((req, res, next) => {
+        res.status(404).send("Sorry can't find that!")
+    })
+
+
+    // connect to port
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log('connected to port');
+    })
+};
+
+
+
+startApp();
+
+
+
+
+
+
